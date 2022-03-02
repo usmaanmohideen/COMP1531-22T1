4abcec05a44bb73b62d92c1fdfef3b6f9a7c5b49
usmaan23 <usmaan.m023@gmail.com>
2022-03-03 08:48:33 +1100

    Before tut exercises

---
+++ w3/B.py
@@ -0,0 +1,15 @@
+'''
+Group Activity
+Your tutor will break you up into groups,
+with people who you are not in the same project team as you.
+
+You will have around 10 minutes to share:
+
+    When you are running standups and whether they are synchronous or asynchronous
+    How often you meet, how you meet, and what the goals/outcomes of any meetings so far have been
+    Have they or will you try pair programming
+    Any challenges you've faced already after being in a group
+
+After that time, share thoughts among the class.
+
+'''
---
+++ w3/C.py
@@ -0,0 +1,28 @@
+'''
+Write a python program vowel.py that
+
+takes in a series of words on a single line in from STDIN,
+passes that input string into a function called find_vowels,
+which then return the frequency of how often each vowel appears.
+This object is then outputted (however you think works best).
+
+Assume all input is lowercase or uppercase letters, or spaces.
+
+If there are any uppercase letters
+passed in as part of the input,
+a ValueError should be thrown
+
+and caught in the main part of the code.
+The user should then be given a clear error
+
+First, write a failing unit test that asserts the error is thrown
+Then modify your code so that the test passes
+Then, update the if __name__ block to catch the exceptional case.
+'''
+
+def find_vowels(message):
+    pass
+
+# https://www.freecodecamp.org/news/if-name-main-python-example/
+if __name__ == '__main__':
+    pass
